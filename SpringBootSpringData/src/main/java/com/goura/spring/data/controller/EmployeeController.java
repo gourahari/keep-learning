1facfc8b6474df76583ffbb906b226ae7714a73f
Gourahari Das <gourahari.das@gmail.com>
2018-01-31 15:04:55 -0800

    Rest controller and error handling.

--- SpringBootSpringData/src/main/java/com/goura/spring/data/controller/EmployeeController.java
+++ SpringBootSpringData/src/main/java/com/goura/spring/data/controller/EmployeeController.java
@@ -1,14 +1,20 @@
 package com.goura.spring.data.controller;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.goura.spring.data.bean.Employee;
+import com.goura.spring.data.error.NotFoundException;
+import com.goura.spring.data.error.RestException;
 import com.goura.spring.data.repository.EmployeeRepository;
 
 @RestController
@@ -19,7 +25,28 @@ public class EmployeeController {
 	private EmployeeRepository repository;
 
 	@GetMapping("/employees")
+	@ResponseStatus(HttpStatus.OK)
 	public List<Employee> getAll() {
-		return Arrays.asList();
+		Iterable<Employee> iterable = repository.findAll();
+		List<Employee> target = new ArrayList<>();
+		iterable.forEach(target::add);
+		return target;
+	}
+
+	@GetMapping("/employees/id/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Employee getById(@PathVariable Long id) throws RestException {
+		return repository.findById(id)
+				.orElseThrow(() -> new NotFoundException(String.format("Employee with id: [%s] doesn't exist.", id)));
+	}
+
+	@DeleteMapping("/employees/id/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteById(@PathVariable Long id) throws RestException {
+		if (repository.existsById(id)) {
+			repository.deleteById(id);
+		} else {
+			throw new NotFoundException(String.format("Employee with id: [%s] doesn't exist.", id));
+		}
 	}
 }
