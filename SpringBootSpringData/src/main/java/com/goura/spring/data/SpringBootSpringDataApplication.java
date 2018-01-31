2472a04948f905d531cafe9c085281975e36f27a
Gourahari Das <gourahari.das@gmail.com>
2018-01-30 17:49:14 -0800

    initial version

---
+++ SpringBootSpringData/src/main/java/com/goura/spring/data/SpringBootSpringDataApplication.java
@@ -0,0 +1,12 @@
+package com.goura.spring.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpringDataApplication.class, args);
+	}
+}
